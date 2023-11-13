ced5aa4d31c091b574d4a494c4699f3da2c7ee68
Soumyakanti Pan <soumyakanti8051@gmail.com>
2023-11-12 23:57:11 -0800

    spBayes runtime

--- hoffman/test/h_runtime_comparison.R
+++ hoffman/test/h_runtime_comparison.R
@@ -2,11 +2,14 @@ library(spBayes)
 
 dat <- read.csv("../data/h_sim_count1000.csv")
 
-runtime <- array(dim = 5)
-nseq <- c(1:5)*100
+n_run <- 2
+runtime <- array(dim = c(n_run, 2))
+nseq <- c(1:n_run)*100
+runtime[, 1] <- nseq
+colnames(runtime) <- c("n", "time")
 
-for(i in 1:5){
-  simdat <- dat[1:nseq[i], ]
+for(i in 1:n_run){
+  simdat <- dat[nseq[i], ]
   y <- as.numeric(simdat$y)
   X <- as.matrix(simdat[, grep("x", names(simdat))])
   X <- X[, -1]
@@ -22,11 +25,12 @@ for(i in 1:5){
   m.1 <- spGLM(y~X-1, family="poisson", coords=S,
                starting=list("beta"=beta.starting, "phi"=0.06,"sigma.sq"=1, "w"=0),
                tuning=list("beta"=0.1, "phi"=0.5, "sigma.sq"=0.5, "w"=0.5),
-               priors=list("beta.Flat", "phi.Unif"=c(0.03, 0.3), "sigma.sq.IG"=c(2, 1)),
+               priors=list("beta.Flat", "phi.Unif"=c(0.03, 10), "sigma.sq.IG"=c(2, 1)),
                amcmc=list("n.batch"=n.batch, "batch.length"=batch.length, "accept.rate"=0.43),
                cov.model="exponential", verbose=TRUE, n.report=10)
   t2 <- Sys.time()
-  runtime[i] <- t2-t1
+  runtime[i, 2] <- t2-t1
   cat("n = ", nseq[i], " time = ", t2-t1, "\n")
 }
 
+write.csv(runtime, "spBayes_runtime.txt")
