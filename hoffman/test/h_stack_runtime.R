f674d69909bc48bf4330e5d175c4ace3174eb93f
Soumyakanti Pan <soumyakanti8051@gmail.com>
2023-11-13 02:30:53 -0800

    stack trial job

---
+++ hoffman/test/h_stack_runtime.R
@@ -0,0 +1,39 @@
+rm(list = ls())
+
+source("../src/h_runsrc.R")
+
+dat <- read.csv("../data/h_sim_count5000.csv")
+
+# n_run <- 14
+n_run <- 2
+runtime <- array(dim = c(n_run, 2))
+# nseq <- c(c(1:10)*100, 2000, 3000, 4000, 5000)
+nseq
+runtime[, 1] <- nseq
+colnames(runtime) <- c("n", "time")
+
+n_postsamp <- 500
+
+mod_list <- create_model_list(G_decay = c(3, 4, 10), 
+                              G_smoothness = c(0.5, 1, 1.5),
+                              G_epsilon = c(0.25, 0.5),
+                              G_nuxi = 0,
+                              G_nubeta = 2.1, G_nuz = 2.1)
+
+for(i in 1:n_run){
+  simdat <- dat[1:nseq[i], ]
+  y <- as.numeric(simdat$y)
+  X <- as.matrix(simdat[, grep("x", names(simdat))])
+  S <- as.matrix(simdat[, c("s1", "s2")])
+  
+  t1 <- Sys.time()
+  m_out <- spGLM_stack(y = y, X = X, S = S, N.samp = n_postsamp,
+                       family = "poisson",
+                       spCov = "matern",
+                       mod_params_list = mod_list,
+                       verbose = FALSE)
+  t2 <- Sys.time()
+  runtime[i, 2] <- t2-t1
+}
+
+write.csv(runtime, "stack_runtime.csv", row.names = FALSE)
--- hoffman/test/runtime_manual.R
+++ hoffman/test/runtime_manual.R
@@ -1,8 +1,39 @@
+library(latex2exp)
+library(ggplot2)
 
 nseq <- c(1:5)*100
 
-stack_fit <- c(15.1, 25.2, 38.3, 57.2) # 75.6 99, 115.8, 138.0, 156.0, 198
-stack_nofit <- c(10.3, 17.8, 25.6, 40.8) # 45.1 55.7, 73.8, 90.6, 105.6, 126.6
-spBayes_time <- c(55.4, 381.6, 981.6)
+stack_fit <- c(15.1, 25.2, 38.3, 57.2, 75.6) # 99, 115.8, 138.0, 156.0, 198
+stack_nofit <- c(10.3, 17.8, 25.6, 40.8, 45.1) # 55.7, 73.8, 90.6, 105.6, 126.6
+spBayes_time <- c(55.4, 381.6, 981.6, 2469, 4968)
 
-run_time <- data.frame()
+run_time <- data.frame(n = nseq, stack_nofit = stack_nofit,
+                       stack_fit = stack_fit, spBayes_time = spBayes_time)
+
+library("tidyverse")
+rt <- run_time %>%
+  select(n, stack_nofit, stack_fit, spBayes_time) %>%
+  gather(key = "variable", value = "value", -n)
+
+ggplot(rt, aes(x = n, y = value)) +
+  geom_line(aes(color = variable)) +
+  geom_point(aes(color = variable, alpha = 0.5)) +
+  scale_y_continuous(trans='log10') +
+  xlab(TeX('$n$')) +
+  ylab(TeX('$\\log_{10}$time')) +
+  labs(color = "Method") +
+  scale_color_discrete(labels = c("spBayes", "stack.fit", "stack")) +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.background = element_blank(),
+        legend.position = c(0.12, 0.99),
+        legend.text = element_text(size = 10, family = "mono"),
+        legend.key.size = unit(0.5, 'cm'),
+        axis.title.x = element_text(size = 12, family = "mono"),
+        axis.title.y = element_text(size = 12, family = "mono"),
+        # axis.line = element_line(color='black'),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        # panel.border = element_blank(),
+        aspect.ratio = 0.67)
