c3cc86a9fdfe1de1cd7b645a9fd7a1f2b97c881a
Soumyakanti Pan <soumyakanti8051@gmail.com>
2023-11-27 22:44:42 -0800

    spBayes w

---
+++ hoffman2/h_spBayes_Poisson.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+library(spBayes)
+
+# library(ggplot2)
+# library(MBA)
+# library(latex2exp)
+# source("../src/pointrefplot.R")
+
+dat <- read.csv("../data/sim_count1000.csv")
+simdat <- dat[1:50, ]
+y <- as.numeric(simdat$y)
+X <- as.matrix(simdat[, grep("x", names(simdat))])
+X <- as.matrix(X[, -1])
+S <- as.matrix(simdat[, c("s1", "s2")])
+rm(dat)
+
+##Collect samples
+beta.starting <- coefficients(glm(y~X, family="poisson"))
+beta.tuning <- t(chol(vcov(glm(y~X, family="poisson"))))
+n.batch <- 500
+batch.length <- 50
+n.samples <- n.batch*batch.length
+
+m.1 <- spGLM(y~X, family="poisson", coords=S,
+             starting=list("beta"=beta.starting, "phi"=0.06,"sigma.sq"=1, "w"=0),
+             tuning=list("beta"=c(0.1, 0.1), "phi"=0.5, "sigma.sq"=0.5, "w"=0.5),
+             priors=list("beta.Flat", "phi.Unif"=c(0.03, 10), "sigma.sq.IG"=c(2, 1)),
+             amcmc=list("n.batch"=n.batch, "batch.length"=batch.length, "accept.rate"=0.43),
+             cov.model="exponential", verbose=TRUE, n.report=10)
+
+burn.in <- 0.9*n.samples
+sub.samps <- burn.in:n.samples
+
+w.hat <- m.1$p.w.samples[,sub.samps]
+write.table(w.hat, file = "posterior_spatial.csv", 
+            col.names = FALSE, row.names = FALSE)
+
+# w_hat <- read.table("posterior_spatial.csv", header = FALSE)
+# simdat$postmedian_z <- apply(w_hat, 1, median)
+# 
+# leg_title <- TeX('$z(s)$')
+# p <- pointref_plot(simdat, "postmedian_z", legend_title = leg_title)
+# 
+# ggsave("postmedian_z_pois.pdf", plot = p6, width=4, height=4, units='in')
---
+++ hoffman2/submit_spBayes_pois.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#$ -cwd #uses current working directory
+# error = Merged with joblog
+#$ -o joblog.$JOB_ID #creates a file called joblog.jobidnumber to write to.
+#$ -j y
+#$ -l h_data=3G,h_rt=6:00:00,highp #requests 30 minutes, 2GB of data (per core)
+#$ -pe shared 1 #requests 2 cores
+# Email address to notify
+#$ -M $USER@mail #don't change this line, finds your email in the system
+# Notify when
+#$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+module load R/4.2.2 #loads R for use
+
+# run R code
+echo 'Running h_runtime_comparison.R' #prints this quote to joblog.jobidnumber
+Rscript h_spBayes_pois.R > output.$JOB_ID 2>&1
+# command-line arguments: number of samples, number of repetitions, seed, command to create n samples
+# outputs any text (stdout and stderr) to output.$JOB_ID
