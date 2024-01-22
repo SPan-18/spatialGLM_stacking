42548d75cb6985b4ef5b7a6b58556aea262fa61c
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-01-22 11:22:19 -0800

    mcse

--- hoffman2/credible_plot.R
+++ hoffman2/credible_plot.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 
 # beta_MCMC1 <- read.table("post_spBayes/beta_hat_100.txt")
-beta_MCMC1 <- read.table("post_MCMC/beta_100.txt")
+beta_MCMC1 <- read.table("post_MCMC_jan21/beta_100.txt")
 beta_MCMC1 <- as.data.frame(t(as.matrix(beta_MCMC1)))
 # beta_MCMC2 <- read.table("post_spBayes/beta_hat_500.txt")
 
@@ -64,7 +64,7 @@ library(ggplot2)
 # pal <- wesanderson::wes_palette("Darjeeling1", 2, type = "discrete")
 p1 <- ggplot(data = post_beta100, aes(x = beta0)) +
   geom_density(aes(fill = Method, color = Method), alpha = 0.3) +
-  xlim(0, 10) +
+  # xlim(0, 10) +
   # scale_color_manual(values = pal) +
   # scale_fill_manual(values = pal) +
   theme_bw() +
@@ -86,7 +86,7 @@ p2 <- ggplot(data = post_beta100, aes(x = beta1)) +
   geom_density(aes(fill = Method, color = Method), alpha = 0.3) +
   # scale_color_manual(values = c("#B4AF46", "#4682B4")) +
   # scale_fill_manual(values = c("#B4AF46", "#4682B4")) +
-  xlim(-1.5, 0.5) +
+  # xlim(-1.5, 0.5) +
   theme_bw() +
   xlab(latex2exp::TeX('$\\beta_1$')) +
   geom_point(aes(x = -0.5, y = 0),
@@ -110,4 +110,17 @@ gridExtra::grid.arrange(p1, p2, ncol = 2)
 #   geom_density(aes(fill = Method), alpha = 0.2) +
 #   facet_wrap(~ variable, scales = "free_x")
 
+# qq <- as.data.frame(qqplot(beta_MCMC1[, 1], 
+#                            beta_stack1$beta0, 
+#                            plot.it = FALSE))
+# ggplot(qq) + 
+#   geom_point(aes(x = x, y = y))
+
+
+
+# phi_MCMC <- read.table("post_MCMC_jan21/phi_100.txt")
+# nu_MCMC <- read.table("post_MCMC_jan21/nu_100.txt")
+# 
+# plot(density(phi_MCMC[, 1]))
+# plot(density(nu_MCMC[, 1]))
 
